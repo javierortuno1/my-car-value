1b45129903459646bbb2b9c3639a8fb5007219dc
Javier Ortuno <javier.ortuno@akka.eu>
2022-11-30 12:25:23 +0100

    Unit Test: AuthService - Refactoring

--- src/users/auth/auth.service.spec.ts
+++ src/users/auth/auth.service.spec.ts
@@ -3,35 +3,39 @@ import { User } from "../user.entity";
 import { UsersService } from "../users.service";
 import { AuthService } from "./auth.service"
 
-// A test block
-it('can create an instance of auth service', async () => {
-    // Fake copy of users service
-    const fakeUsersService: Partial<UsersService> = {
-        find: () => Promise.resolve([]),
-        create: (email: string, password: string) => {
-            // 'as User' as a way to indicate TS to treat the object as a UserObject. -> Casting
-            return Promise.resolve({ id: 1, email, password } as User) 
-        }
-            
-        
-    }
+describe('AuthService', () => {
+    let service: AuthService;
 
-    // We create a module for the testing feature of the auth service
-        // PRoviders: List of things we want to registe in our testing DI container 
-            // provide: If anyone asks for the UsersService, then use the value of 'useValue'
-    const module = await Test.createTestingModule({
-        providers: [
-            AuthService,
-            {
-                provide: UsersService,
-                useValue: fakeUsersService
+    beforeEach(async () => {
+        // Fake copy of users service
+        const fakeUsersService: Partial<UsersService> = {
+            find: () => Promise.resolve([]),
+            create: (email: string, password: string) => {
+                // 'as User' as a way to indicate TS to treat the object as a UserObject. -> Casting
+                return Promise.resolve({ id: 1, email, password } as User)
             }
-        ]
-    }).compile();
+        }
 
-    // we create the service
-    const service = module.get(AuthService);
+        // We create a module for the testing feature of the auth service
+            // PRoviders: List of things we want to registe in our testing DI container
+                // provide: If anyone asks for the UsersService, then use the value of 'useValue'
+        const module = await Test.createTestingModule({
+            providers: [
+                AuthService,
+                {
+                    provide: UsersService,
+                    useValue: fakeUsersService
+                }
+            ]
+        }).compile();
 
-    // we validate that the service exist
-    expect(service).toBeDefined();
+        // we create the service
+        service = module.get(AuthService);
+    })
+
+    // A test block
+    it('can create an instance of auth service', async () => {
+        // we validate that the service exist
+        expect(service).toBeDefined();
+    })
 })
