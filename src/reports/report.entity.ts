5c87270855001bbf0c54781dc0780b905446ada3
Javier Ortuno <javier.ortuno@akka.eu>
2022-12-24 14:23:03 +0100

    Relations with TypeORM: (Define Associations) Associate a user with the reports they create. 1- Find out what kind of association we are modeling. In this case it will be One(User)-to-Many(Reports). 2- Add the appropriate decorators to our related entities. 2.1- Define a new propery with @OneToMany for the User Entity and @ManyToOne for the Report Entity as a way to indicate association with a Target Entity. 3- Associations are not automatically fetched when we fetch an Entity but the property exist, then we have to define the associtaion property in the query

--- src/reports/report.entity.ts
+++ src/reports/report.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { User } from 'src/users/user.entity';
+import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 
 // Their Repository is created by connecting the entity to the parent Module.
 @Entity()
@@ -30,5 +31,12 @@ export class Report {
     // Latitude
     @Column()
     lat: number;
+
+    // Pass as argument 1- the Entity we are expecting to be associated as function
+    // for avoiding circular dependency
+    // 2- and the instance of that entity with the property we wish to associate
+    // NOTE: Many to one is going to alter the Report Table 
+    @ManyToOne(() => User, (user) => user.reports)
+    user: User;
 
 }
